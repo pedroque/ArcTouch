27d008de3bcf915bdd0d10fe540cb7bdf60ff8e3
pedroabinajm <pedroabinajm@gmail.com>
2018-01-08 23:08:43 -0200

    Show just the year in item movie

--- app/src/main/java/com/arctouch/test/data/model/Movie.kt
+++ app/src/main/java/com/arctouch/test/data/model/Movie.kt
@@ -1,13 +1,25 @@
 package com.arctouch.test.data.model
 
+import android.databinding.BaseObservable
+import android.databinding.Bindable
 import java.util.*
 
 
-class Movie {
+class Movie : BaseObservable(){
     var title: String? = null
     var posterPath: String? = null
     var backdropPath: String? = null
     var releaseDate: Date? = null
     var overview: String? = null
     lateinit var genreIds: MutableList<Long>
+
+    @Bindable
+    fun getReleaseYear(): String {
+        releaseDate?.let {
+            val calendar = Calendar.getInstance()
+            calendar.time = it
+            return calendar.get(Calendar.YEAR).toString()
+        }
+        return ""
+    }
 }
